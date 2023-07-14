d8303d5849986ea6a62f9d661b576f8c8f15d349
Valeria <112584734+valgz@users.noreply.github.com>
2023-07-14 15:05:51 +0200

    Update streamlit_app.py
    fixing

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,12 +36,12 @@ def get_fruityvice_data(this_fruit_choice):
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruir to get information.")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(brack_from_function)
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+          streamlit.error("Please select a fruit to get information.")
+    else:
+          back_from_function = get_fruityvice_data(fruit_choice)
+          streamlit.dataframe(brack_from_function)
 
 except URLError as e:
   streamlit.error()
